38e28d5cf881a5e7c354db58f8a2b3597ca3dc05
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-12-11 23:44:03 +0700

    .
    .

--- config-overrides.js
+++ config-overrides.js
@@ -2,13 +2,10 @@ const { override, addWebpackPlugin } = require('customize-cra');
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = override(
-  // Thêm plugin nén vào config của webpack,
- 
-  addWebpackPlugin(new CompressionPlugin()),
-  (config) => {
-    if (config.devServer) {
-      config.devServer.allowedHosts = ['https://web-service-booking-tour.onrender.com'];
-    }
-    return config;
-  }
+    (config) => {
+        if (config.devServer) {
+          config.devServer.allowedHosts = ['web-service-booking-tour.onrender.com'];
+        }
+        return config;
+      }
 );
