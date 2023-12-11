38b8a6f7cd495d36fd7ca3dc0a1e6a5c30ff17a3
Nguyễn Văn Hơn <74673759+Hon1802@users.noreply.github.com>
2023-12-11 20:54:29 +0700

    .
    .

--- src/constants/base-constant.js
+++ src/constants/base-constant.js
@@ -1,5 +1,5 @@
 export const BASE_CONSTANT = {
-    BASE_URL: process.env.REACT_APP_BASE_URL || "https://web-service-booking-tour.onrender.com/",
+    BASE_URL: process.env.REACT_APP_BASE_URL || "https://web-service-booking-tour.onrender.com",
     MONGO_URI: process.env.REACT_APP_MONGO_URI,
     JWT_SECRET: process.env.REACT_APP_JWT_SECRET
 }
